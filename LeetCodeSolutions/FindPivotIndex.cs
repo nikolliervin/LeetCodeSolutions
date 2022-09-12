fbe96870ab88f5190e0bd1babe075be9d540c035
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-09-12 20:00:02 +0200

    Pivot Index Solution

--- LeetCodeSolutions/FindPivotIndex.cs
+++ LeetCodeSolutions/FindPivotIndex.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LeetCodeSolutions
+﻿namespace LeetCodeSolutions
 {
     internal class FindPivotIndex
     {
+        public int PivotIndex(int[] array)
+        {
+            int sum = 0;
+            int leftSum = 0;
+            if (array.Length == 0)
+                return -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int rightSum = sum - leftSum - array[i];
+
+                if (rightSum == leftSum)
+                    return i;
+                leftSum += array[i];
+            }
+            return -1;
+
+
+        }
     }
 }
--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            int[] array = new int[] { 1, 7, 3, 6, 5, 6 };
 
-            RunningSumOf1DArray RuuningSum = new RunningSumOf1DArray();
-            RuuningSum.RunningSumOfArray(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+
+            }
+
+
 
 
 
