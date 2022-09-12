d7667cf87f80d1b904b15444a6f6524bf6e695ec
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-09-12 13:43:56 +0200

    Running sum of an array

--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace LeetCodeSolutions
+﻿namespace LeetCodeSolutions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            RunningSumOf1DArray RuuningSum = new RunningSumOf1DArray();
+            RuuningSum.RunningSumOfArray(array);
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
     }
 }
--- LeetCodeSolutions/RunningSumOf1DArray.cs
+++ LeetCodeSolutions/RunningSumOf1DArray.cs
@@ -1,10 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LeetCodeSolutions
 {
     internal class RunningSumOf1DArray
     {
+        public void RunningSumOfArray(int[] nums)
+        {
+            int[] array = new int[nums.Length];
+            array[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                array[i] = array[i - 1] + nums[i];
+
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+        }
     }
+
+
 }
